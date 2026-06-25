dfd2e5416bb2b735401535396ce7fe9a65cba416
zBrunooDev <zbrunolima513@gmail.com>
2026-06-25 17:54:51 -0300

    Adicionando novo exercicio de lógica

---
+++ LogicaDeProgramacao/LogicaDeProgramacao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LogicaDeProgramacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EX 1.1
+
+            /*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+            código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2.Calcule e mostre o valor a ser pago.*/
+
+            double[,] mat = new double[2, 3];
+            double soma = 0;
+
+            for(int i = 0; i < 2; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < 3; j++)
+                {
+                    mat[i, j] = double.Parse(s[j]);
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                soma += mat[i, 1] * mat[i, 2];
+            }
+
+            Console.WriteLine($"VALOR A PAGAR: R${soma:F2}");
+
+        }
+    }
+}
---
+++ LogicaDeProgramacao/LogicaDeProgramacao/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("LogicaDeProgramacao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicaDeProgramacao")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("2b1f25f6-e4a1-4bd3-a574-15057f506de8")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
