76b83d5a89248b0ac6aea210727b2e053daaa9c8
zBrunooDev <zbrunolima513@gmail.com>
2026-07-15 22:27:39 -0300

    Resolvido exercicio 1051 Beecrowd

--- Beecrowd/Ex1051/Ex1051/Program.cs
+++ Beecrowd/Ex1051/Ex1051/Program.cs
@@ -10,7 +10,35 @@ namespace Ex1051
     {
         static void Main(string[] args)
         {
-            
+            double salario = double.Parse(Console.ReadLine());
+
+            if(salario <= 2000)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if(salario > 2000 && salario <= 3000)
+            {
+                double imposto = salario - 2000;
+                imposto = imposto * 0.08;
+                Console.WriteLine($"R$ {imposto:F2}");
+            }
+            else if (salario > 3000 && salario <= 4500)
+            {
+                double imposto = salario - 2000; // Valor insento
+                imposto = imposto - 1000; // valor de 8%
+                imposto = imposto * 0.18 + 80; // imposto da casa dos 18% + 8%
+
+                Console.WriteLine($"R$ {imposto:F2}");
+            }
+            else if (salario > 4500)
+            {
+                double imposto = salario - 2000; // Valor insento
+                imposto -= 1000; // valor de 8%
+                imposto -= 1500; // imposto de 18%
+                imposto = imposto * 0.28 + 80 + 270; // imposto da casa dos 18% + 8%
+
+                Console.WriteLine($"R$ {imposto:F2}");
+            }
         }
     }
 }
