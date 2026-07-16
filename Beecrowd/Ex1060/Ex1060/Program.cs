0635c7b52391ca2cb7bbbaf81aba05551c293708
zBrunooDev <zbrunolima513@gmail.com>
2026-07-15 23:27:36 -0300

    Resolução Exercicio 1060 Beecroewd

---
+++ Beecrowd/Ex1060/Ex1060/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1060
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vet = new double[6];
+            int cont = 0;
+
+            for(int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine());
+                if (vet[i] > 0)
+                {
+                    cont++;
+                }
+            }
+
+            Console.WriteLine($"{cont} valores positivos");
+        }
+    }
+}
