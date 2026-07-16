479a3d8a7536465a6b72ffbd314ba354a84901ff
zBrunooDev <zbrunolima513@gmail.com>
2026-07-15 22:45:46 -0300

    Resolução exercicio 1052 Beecrowd

---
+++ Beecrowd/Ex1052/Ex1052/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1052
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(meses[mes - 1]);
+        }
+    }
+}
