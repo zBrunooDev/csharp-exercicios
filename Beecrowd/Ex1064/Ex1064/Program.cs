ff6c724e79ea16a9a0838b52abef0eb7b8bf1d90
zBrunooDev <zbrunolima513@gmail.com>
2026-07-16 23:17:51 -0300

    Resolução exercicoo 1064 Beecrowd

---
+++ Beecrowd/Ex1064/Ex1064/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1064
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vet = new double[6];
+            int cont = 0, contMedia = 0;
+            double somaPositivo = 0, media = 0;
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine());
+                if (vet[i] > 0)
+                {
+                    cont++;
+                    contMedia++;
+                    somaPositivo += vet[i]; 
+                }
+            }
+
+            media = somaPositivo / contMedia;
+
+            Console.WriteLine($"{cont} valores positivos");
+            Console.WriteLine($"{media:F1}");
+
+        }
+    }
+}
