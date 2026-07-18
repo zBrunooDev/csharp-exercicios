474c42e4be1d0708ed088a0ce8a2544daf71a185
zBrunooDev <zbrunolima513@gmail.com>
2026-07-17 23:43:39 -0300

    Tentando resolver exercicio 1159 beecrowd

---
+++ Beecrowd/Ex1159/Ex1159/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1159
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int soma = 0, cont =0;
+            int[] vetSoma;
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+                do
+                {
+                    if (n % 2 == 0)
+                    {
+                        soma += n;
+                        cont++;
+                    }
+                    n++;
+                }
+                while (cont < 5);
+                vetSoma[] = soma;
+
+            }while (n == 0);
+
+
+
+        }
+    }
+}
