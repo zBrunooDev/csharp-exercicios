51526661a7f45fa7b2e971a547adb01841e71af6
zBrunooDev <zbrunolima513@gmail.com>
2026-07-19 21:55:33 -0300

    feat: Criando o menu

--- POO/CadastroDeProdutos/CadastroDeProdutos/Program.cs
+++ POO/CadastroDeProdutos/CadastroDeProdutos/Program.cs
@@ -10,6 +10,36 @@ namespace CadastroDeProdutos
     {
         static void Main(string[] args)
         {
+            List<int> produtos = new List<int>();
+
+            Console.WriteLine("=== Cadastro de Produtos ===");
+
+            Console.WriteLine("" +
+                "1 - Cadastrar produto\r\n" +
+                "2 - Listar produtos\r\n" +
+                "3 - Buscar produto\r\n" +
+                "4 - Atualizar estoque\r\n" +
+                "5 - Remover produto\r\n" +
+                "0 - Sair");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            do
+            {
+                switch (opcao)
+                {
+                    case 1:
+
+                        break;
+                }
+
+            }while(true);
+
+        }
+
+        public void CadastrarProduto()
+        {
+            Console.WriteLine("");
         }
     }
 }
