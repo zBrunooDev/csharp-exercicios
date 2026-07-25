69150f6831bb90e14c78619b7e73e030c4deaa24
zBrunooDev <zbrunolima513@gmail.com>
2026-07-24 23:12:58 -0300

    Resolução exercício 1173 Beecrowd

---
+++ Beecrowd/Ex1173/Ex1173/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1173
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vet = new int[10];
+            int n = int.Parse(Console.ReadLine());
+            vet[0] = n;
+            for (int i = 1; i < vet.Length; i++)
+            {
+                vet[i] = vet[i - 1] * 2;
+            }
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+    }
+}
