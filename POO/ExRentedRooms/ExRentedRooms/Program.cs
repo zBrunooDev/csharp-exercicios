16fc8d7337bd5caa82ded05b7661d79476ed5f7a
zBrunooDev <zbrunolima513@gmail.com>
2026-08-06 22:27:43 -0300

    Resolvendo exercicios curso vetor com tipo referência

---
+++ POO/ExRentedRooms/ExRentedRooms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRentedRooms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int rooms = int.Parse(Console.ReadLine());
+
+            Students[] roomsRetend = new Students[10];
+
+            for (int i = 0; i < rooms; i++)
+            {
+                Console.WriteLine($"Rent #{i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                roomsRetend[i] = new Students {Name = name, Email = email, Room = room};
+                Console.WriteLine();
+            }
+
+            for(int i = 0;i < rooms; i++)
+            {
+                Console.WriteLine($"Busy rooms: ");
+                Console.WriteLine($"{roomsRetend[i].Room}: {roomsRetend[i].Name}, {roomsRetend[i].Email}");
+
+            }
+
+        }
+    }
+}
---
+++ POO/ExRentedRooms/ExRentedRooms/Students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRentedRooms
+{
+    internal class Students
+    {
+        public string Name {  get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+    }
+}
