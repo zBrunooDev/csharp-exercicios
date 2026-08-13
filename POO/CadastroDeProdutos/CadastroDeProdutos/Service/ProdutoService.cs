46d434965ffb1d63f0a835db1203985fc71ef805
zBrunooDev <zbrunolima513@gmail.com>
2026-08-12 22:54:15 -0300

    feat: implementando cadastro do produto na service

---
+++ POO/CadastroDeProdutos/CadastroDeProdutos/Service/ProdutoService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeProdutos.Service
+{
+    internal class ProdutoService
+    {
+
+        static List<Produto> produtos = new List<Produto>();
+        static int contId = 1;
+
+        // Cadastrar Produto
+        public Produto CadastrarProduto(string nome, double preco, int quantidade)
+        {
+            contId += 1;
+            return new Produto()
+            {
+                Nome = nome,
+                Preco = preco,
+                Quantidade = quantidade
+            };     
+        }
+        // Listar Produto
+        // Buscar por ID
+        // Deletar Produto
+
+    }
+}
