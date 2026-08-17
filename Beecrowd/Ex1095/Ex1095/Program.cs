02919d3e3f404d2e3b379e64ae940ca6506e3e6c
zBrunooDev <zbrunolima513@gmail.com>
2026-08-16 22:55:36 -0300

    resolução exercicio 1095 Beecrowd

---
+++ Beecrowd/Ex1095/Ex1095/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1095
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 60;
+
+            // O laço continua enquanto J for maior ou igual a 0
+            while (j >= 0)
+            {
+                Console.WriteLine($"I={i} J={j}");
+                i += 3; // I aumenta de 3 em 3
+                j -= 5; // J diminui de 5 em 5
+            }
+        }
+    }
+}
