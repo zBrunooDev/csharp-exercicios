55c11626547e608f73bff6ebbf2c81a94594d8e2
zBrunooDev <zbrunolima513@gmail.com>
2026-08-20 22:54:11 -0300

    feat: creating the list and data entry

--- POO/EmployeeExercice/EmployeeExercice/Program.cs
+++ POO/EmployeeExercice/EmployeeExercice/Program.cs
@@ -10,7 +10,28 @@ namespace EmployeeExercice
     {
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
 
+            Console.Write("How many employees wil be registered? ");
+            int loop = int.Parse(Console.ReadLine());
+
+            Employee employee;
+
+            for( int i = 1; i <= loop; i++)
+            {
+                Console.WriteLine($"Employee #{i}");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: R$ ");
+                double salary = double.Parse(Console.ReadLine());
+                
+                employee = new Employee(id, name, salary);
+
+                employees.Add(employee);
+                
+            }
         }
     }
 }
