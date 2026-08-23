ff4a4a77687afb70d9227dfbea04fccc57f21136
zBrunooDev <zbrunolima513@gmail.com>
2026-08-23 20:45:45 -0300

    review: testing methods of the list class

--- POO/ListNames/ListNames/Program.cs
+++ POO/ListNames/ListNames/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,20 +16,41 @@ namespace ListNames
             
             names.Add("Bruno");
             names.Add("Maysa");
+            names.Add("Bernardo");
             names.Add("Mauruicio");
 
-            foreach(string name in names)
+            foreach (string name in names)
             {
                 Console.WriteLine(name);
             }
 
+            Console.WriteLine();
+
             Console.WriteLine(names.Count);
 
             string n1 = names.Find(x => x[0] == 'B');
-            Console.WriteLine(n1);
+            Console.WriteLine("First: " + n1);
 
+            string n2 = names.FindLast(x => x[0] == 'B');
+            Console.WriteLine("Last: " + n2);
 
+            Console.WriteLine();
 
+            names.Remove(names[2]);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.RemoveAll(x => x[0] == 'B');
+
+            Console.WriteLine();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
 
         }
     }
