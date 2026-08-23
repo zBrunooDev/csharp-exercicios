05ee6f9091c6fc877db89f046f348a60133ec71b
zBrunooDev <zbrunolima513@gmail.com>
2026-08-23 20:30:35 -0300

    wip: saving progress to continue on the notebook

---
+++ POO/ListNames/ListNames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> names = new List<string>();
+            
+            names.Add("Bruno");
+            names.Add("Maysa");
+            names.Add("Mauruicio");
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine(names.Count);
+
+            string n1 = names.Find(x => x[0] == 'B');
+            Console.WriteLine(n1);
+
+
+
+
+        }
+    }
+}
