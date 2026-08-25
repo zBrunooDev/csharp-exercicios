4ec28596507c84506b3c99f5a970ed42e6f14a33
zBrunooDev <zbrunolima513@gmail.com>
2026-08-24 22:24:48 -0300

    study: praticando DateTime

---
+++ LogicaDeProgramacao/DateTimeStudy/DateTimeStudy/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime inicio = new DateTime(2026, 8, 24, 18, 0, 0);
+
+            Console.WriteLine(inicio.ToString());
+
+            DateTime sumHours = inicio.AddHours(02);
+
+            Console.WriteLine(sumHours.ToString());
+
+            DateTime sumMinutes = inicio.AddMinutes(30);
+
+            Console.WriteLine(sumMinutes.ToString());
+
+            Console.WriteLine(inicio.DayOfWeek);
+
+            Console.WriteLine(sumHours.Hour);
+
+
+        }
+    }
+}
