54e4a432d683bf7f524d3150aae9b8c006dbadd3
zBrunooDev <zbrunolima513@gmail.com>
2026-08-25 22:41:59 -0300

    docs: estudo de manipulação de DateTime e cálculo de intervalo em C#

---
+++ LogicaDeProgramacao/DateTimeEx/DateTimeEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite seu ano de nascimento: ");
+            string nascimento = Console.ReadLine();
+
+            DateTime date = DateTime.Now;
+            DateTime nascimentoFormatada;
+
+            if (DateTime.TryParse(nascimento, out nascimentoFormatada))
+            {
+                Console.WriteLine(nascimentoFormatada.ToString("dd/MM/yyyy"));
+
+                int idade = date.Year - nascimentoFormatada.Year;
+                if (nascimentoFormatada.Date > date.AddYears(-idade))
+                {
+                    idade--;
+                }
+                Console.WriteLine($"Idade: {idade} anos");
+            }
+            else
+            {
+                Console.WriteLine("Data de nascimento inválida.");
+            }
+        }
+    }
+}
