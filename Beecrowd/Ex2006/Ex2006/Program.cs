6462ad90d50f016a3b99717f8439dee91ac81002
zBrunooDev <zbrunolima513@gmail.com>
2026-08-26 21:48:38 -0300

    study: resolução exercicio 2006 beecrowd

---
+++ Beecrowd/Ex2006/Ex2006/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tea = int.Parse(Console.ReadLine());
+
+            int[] chose = new int[5];
+
+            string[] strings = Console.ReadLine().Split(' ');
+
+            int contWon = 0;
+
+            for (int i = 0; i < chose.Length; i++)
+            {
+                chose[i] = int.Parse(strings[i]);
+                if (tea == chose[i])
+                {
+                    contWon++;
+                }
+            }
+
+            Console.WriteLine(contWon);
+        }
+    }
+}
--- LogicaDeProgramacao/OfficeSystem/OfficeSystem/Program.cs
+++ LogicaDeProgramacao/OfficeSystem/OfficeSystem/Program.cs
@@ -10,6 +10,11 @@ namespace OfficeSystem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sitema  de marcação de tempo");
+
+            Console.Write("Entrada: ");
+            DateTime entrada = DateTime.Parse(Console.ReadLine());
+
         }
     }
 }
