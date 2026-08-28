a9da4223ebd574d5fd35e572a40fef77a56b7214
zBrunooDev <zbrunolima513@gmail.com>
2026-08-28 13:39:36 -0300

    feat: adicionando classe Funcionario

---
+++ LogicaDeProgramacao/OfficeSystem/OfficeSystem/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeSystem
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public TimeSpan CargaHoraria { get; set; }
+        public double Salario { get; set; }
+        public string Cargo { get; set; }
+
+    }
+}
