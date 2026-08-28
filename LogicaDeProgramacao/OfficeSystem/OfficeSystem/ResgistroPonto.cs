2af71cec3a540f353e740944163c886925bdd52c
zBrunooDev <zbrunolima513@gmail.com>
2026-08-28 14:10:05 -0300

    feat: criando classe RegistroPonto

---
+++ LogicaDeProgramacao/OfficeSystem/OfficeSystem/ResgistroPonto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeSystem
+{
+    internal class ResgistroPonto
+    {
+        DateTime Entrada { get; set; }
+        DateTime SaidaAlmoco { get; set; }
+        DateTime VoltaAlmoco { get; set; }
+        DateTime Saida {  get; set; }
+
+    }
+}
