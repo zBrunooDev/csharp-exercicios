19c7b2a5ca650e91192c11cd147b9a7fa5820bb4
zBrunooDev <zbrunolima513@gmail.com>
2026-08-29 23:45:51 -0300

    feat: adiciona regras de calculo ao registro de ponto

--- LogicaDeProgramacao/OfficeSystem/OfficeSystem/ResgistroPonto.cs
+++ LogicaDeProgramacao/OfficeSystem/OfficeSystem/ResgistroPonto.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace OfficeSystem
 {
     internal class ResgistroPonto
     {
-        DateTime Entrada { get; set; }
-        DateTime SaidaAlmoco { get; set; }
-        DateTime VoltaAlmoco { get; set; }
-        DateTime Saida {  get; set; }
+        public Funcionario Funcionario {  get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime SaidaAlmoco { get; set; }
+        public DateTime VoltaAlmoco { get; set; }
+        public DateTime Saida { get; set; }
 
+        public TimeSpan CalcularTempoTrabalhodo()
+        {
+            TimeSpan primeiroPeriodo = SaidaAlmoco - Entrada;
+            TimeSpan segundoPeriodo = Saida - VoltaAlmoco;
+            return primeiroPeriodo + segundoPeriodo;
+        }
+        public TimeSpan CalcularTempoAlmoco()
+        {
+            return VoltaAlmoco - SaidaAlmoco;
+        }
+
+        public TimeSpan CalcularBancoHoras()
+        {
+            return CalcularTempoTrabalhodo() - Funcionario.CargaHoraria;
+        }
     }
 }
