4f7d6f3bfe5c3d5eaffa1fb6de69e41fac102ea7
zBrunooDev <zbrunolima513@gmail.com>
2026-08-31 22:22:14 -0300

    feat: inicializando a service do ponto e adicionando o create

---
+++ LogicaDeProgramacao/OfficeSystem/OfficeSystem/PontoService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeSystem
+{
+    internal class PontoService
+    {
+        List<RegistroPonto> registroPontos = new List<RegistroPonto>();
+
+        //  Criando o CRUD
+        // -> Creat, Read, Update, Delete
+        // Criando um objeto 
+        public RegistroPonto CriarRegistro(Funcionario funcionario, DateTime entrada, DateTime saidaAlmoco, DateTime voltaAlmoco, DateTime saida)
+        {
+            return new RegistroPonto(funcionario)
+            {
+                Entrada = entrada,
+                SaidaAlmoco = saidaAlmoco,
+                VoltaAlmoco = voltaAlmoco,
+                Saida = saida
+            };
+        }
+        public void AdicionarPonto(RegistroPonto registroPonto)
+        {
+            registroPontos.Add(registroPonto);
+        }
+        
+    }
+}
