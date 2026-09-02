2067dd37808213290be6cad0c7cdba9ed25a07ad
zBrunooDev <zbrunolima513@gmail.com>
2026-09-01 22:38:43 -0300

    feat: create enum class OrderStatus

---
+++ POO/EnumStudy/EnumStudy/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStudy.Entities.Enums
+{
+    internal class OrderStatus
+    {
+        // Declaring an object lifecycle
+
+        enum Status : int {
+
+            // Here, the object's statuses will be defined.
+            PendingPayment = 1,
+            Processing = 2,
+            Shipped = 3,
+            Delivered = 4
+        }
+
+
+    }
+}
