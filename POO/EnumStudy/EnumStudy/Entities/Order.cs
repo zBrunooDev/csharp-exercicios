d165cb4c54ee40cb75ccc8f9bdc5fa95467c53ee
zBrunooDev <zbrunolima513@gmail.com>
2026-09-01 22:42:08 -0300

    feat: creating the order entity with the enum property

---
+++ POO/EnumStudy/EnumStudy/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using EnumStudy.Entities.Enums;
+
+namespace EnumStudy.Entities
+{
+    internal class Order
+    { 
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; } // Property that will receive the enum status 
+    }
+}
