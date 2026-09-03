b06f6ea8989b2424f01fc864496b902fb6bbc005
zBrunooDev <zbrunolima513@gmail.com>
2026-09-02 22:43:35 -0300

    feat: criando classe Tarefa

---
+++ POO/EnumStudy2/EnumStudy2/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStudy2
+{
+    internal class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public PrioridadeEnum Prioridade { get; set; }
+
+    }
+}
