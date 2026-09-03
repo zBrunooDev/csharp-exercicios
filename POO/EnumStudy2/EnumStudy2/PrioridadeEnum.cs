f834307970efd646e57220cf07db6450cd4ab271
zBrunooDev <zbrunolima513@gmail.com>
2026-09-02 22:39:42 -0300

    feat: criando enum PrioridadeEnum

---
+++ POO/EnumStudy2/EnumStudy2/PrioridadeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStudy2
+{
+    enum PrioridadeEnum : int
+    {
+        Baixa = 0,
+        Media = 1,
+        Alta = 2,
+        Urgente = 3,
+    }
+}
