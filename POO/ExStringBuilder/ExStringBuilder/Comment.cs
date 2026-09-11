a2c68475da151eada01b4007ad9de82dfd355bf0
zBrunooDev <zbrunolima513@gmail.com>
2026-09-11 18:56:50 -0300

    feat: create class Comment

---
+++ POO/ExStringBuilder/ExStringBuilder/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExStringBuilder
+{
+    internal class Comment
+    {
+        string Text { get; set; }
+
+        Comment() { }
+
+        Comment(string text) { Text = text; }
+
+    }
+}
