ca81b87d427d2d963596b37d96c243ce96471034
zBrunooDev <zbrunolima513@gmail.com>
2026-09-10 22:21:51 -0300

    feat: implementing method Income

--- POO/WorkerContracts/WorkerContracts/Entities/Worker.cs
+++ POO/WorkerContracts/WorkerContracts/Entities/Worker.cs
@@ -34,5 +34,19 @@ namespace WorkerContracts
         {
             Contracts.Remove(contract);
         }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+
     }
 }
