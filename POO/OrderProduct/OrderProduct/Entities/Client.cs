93123b68b76c766c8128a29bf09b129ba6369de0
zBrunooDev <zbrunolima513@gmail.com>
2026-09-12 14:29:08 -0300

    feat: creating entitie Client

---
+++ POO/OrderProduct/OrderProduct/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProduct.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+    }
+
+}
