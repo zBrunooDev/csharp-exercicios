1698425e426fe364e4d5309d6517021f3b8c473b
zBrunooDev <zbrunolima513@gmail.com>
2026-09-12 14:33:19 -0300

    feat: creating entitie Product

---
+++ POO/OrderProduct/OrderProduct/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProduct.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
