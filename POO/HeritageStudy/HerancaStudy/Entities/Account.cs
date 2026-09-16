70706970130ed5a8a7b1a95d54978e45a0a63dbc
zBrunooDev <zbrunolima513@gmail.com>
2026-09-15 21:56:09 -0300

    feat: estudando sobre herança, codigo comentado.

--- POO/HeritageStudy/HerancaStudy/Entities/Account.cs
+++ POO/HeritageStudy/HerancaStudy/Entities/Account.cs
@@ -8,5 +8,32 @@ namespace HerancaStudy.Entities
 {
     internal class Account
     {
+        // Superclass -> Classe base da Herança
+
+        public int Number {  get; set; }
+        public string Holder {  get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Método que faz o saque
+        public void Whithdraw (double amount)
+        {
+            Balance += amount;
+        }
+
+        //Método que realiza o deposito
+        public void Deposit(double amount)
+        {
+            Balance -= amount;
+        }
     }
 }
--- POO/HeritageStudy/HerancaStudy/Entities/BusinessAccount.cs
+++ POO/HeritageStudy/HerancaStudy/Entities/BusinessAccount.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace HerancaStudy.Entities
 {
-    internal class BusinessAccount
+    internal class BusinessAccount : Account // -> Esses dois pontos é uma operaçãao de herança. "BusinessAccount é uma Account"
     {
+        // É nesse momento que as propriedades de BussinesAccount sejam herdadas de Account.
+
+        public double LoanLimit { get; set; } // Aqui eu posso criar outras propriedade que serão apenas dessa subclasse.
+
+        public BusinessAccount() { }
+
+        // Aqui o construtor pode receber os paramentros de entrada do construtor da da  Superclass. Aapenaas utilizando a palavra base.
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            // Agora aqui eu não preciso refazer todos os elementos da classe receberem os valores dos parâmetros novamente, posso reutilizar-los do contrutor da classe base;
+            // Só preciso fazer isso com o novo elemento.
+
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            // Suponha que, para realizar um empréstimo, é descontada uma taxa no valor de 10.0
+
+            if (amount <= LoanLimit)
+            {
+                Balance += amount - 10.0;
+            }
+        }
+
     }
 }
