a798d76cfa18e6efb107091ae39000baa00a849c
zBrunooDev <zbrunolima513@gmail.com>
2026-09-16 22:36:06 -0300

    feat: implementando a classe base Employee e subclasse OutsoucerdEmployee

---
+++ POO/ExPolimorfismoEmployee/ExPolimorfismoEmployee/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExPolimorfismoEmployee.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hour { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+        public Employee(string name, int hour, double valuePerHour)
+        {
+            Name = name;
+            Hour = hour;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hour;
+        }
+
+    }
+}
