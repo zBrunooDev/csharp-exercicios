fed483d23e5b62d64ae48c75bb011cbed3bfc659
zBrunooDev <zbrunolima513@gmail.com>
2026-09-17 23:43:57 -0300

    feat: concluding the project with the exhibition on the program

--- POO/ExImportedProduct/ImportedProduct/Entities/UsedProduct.cs
+++ POO/ExImportedProduct/ImportedProduct/Entities/UsedProduct.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ImportedProduct.Entities
+namespace ExImportedProduct.Entities
 {
     internal class UsedProduct : Product
     {
---
+++ POO/ExImportedProduct/ImportedProduct/Program.cs
@@ -0,0 +1,61 @@
+﻿using ExImportedProduct.Entities; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExImportedProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of  products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (ch == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if(ch == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+
+            foreach(Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+
+        }
+    }
+}
