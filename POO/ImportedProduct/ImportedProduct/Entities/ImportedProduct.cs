5ccdfcaded2653724bf9202a8e0d91c2e0ff3a3b
zBrunooDev <zbrunolima513@gmail.com>
2026-09-17 22:05:16 -0300

    feat: created subclass ImportedProduct

---
+++ POO/ImportedProduct/ImportedProduct/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportedProduct.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + $" Customs free"; 
+        }
+    }
+}
