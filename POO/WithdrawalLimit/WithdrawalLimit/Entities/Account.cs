1cea7839ef8c16d27fed18bcd12970a07744b908
zBrunooDev <zbrunolima513@gmail.com>
2026-09-20 21:30:46 -0300

    feat: create the Account class

---
+++ POO/WithdrawalLimit/WithdrawalLimit/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WithdrawalLimit.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder {  get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account (int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance =+ amount;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance = +amount;
+        }
+    }
+}
