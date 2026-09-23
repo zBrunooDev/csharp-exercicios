bbb1447eb031adf138b86ed03d98eef61cd6b846
zBrunooDev <zbrunolima513@gmail.com>
2026-09-22 23:11:55 -0300

    feat: create Contract and Installment classes

---
+++ POO/ContractsPayment/ContractsPayment/Entities/Contracts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractsPayment.Entities
+{
+    internal class Contracts
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installmensts> Installmensts { get; set; }
+
+        public Contracts(int number, DateTime date, double totalvalue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalvalue;
+        }
+
+
+    }
+}
---
+++ POO/ContractsPayment/ContractsPayment/Entities/Installmensts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractsPayment.Entities
+{
+    internal class Installmensts
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installmensts(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+    }
+}
