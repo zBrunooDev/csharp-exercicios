1545b1a0dc2e354a12d869bcb1824b97fc94de40
zBrunooDev <zbrunolima513@gmail.com>
2026-09-24 22:49:18 -0300

    feat: feat: solve Two Sum problem

--- LeetCode/TwoSum/TwoSum/Program.cs
+++ LeetCode/TwoSum/TwoSum/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace TwoSum
 {
-    internal class Program
+    public class Solution
     {
-        static void Main(string[] args)
+        public int[] TwoSum(int[] nums, int target)
         {
+
+            int[] resultado = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        resultado = new int[] { i, j };
+                    }
+                }
+            }
+            return resultado;
         }
     }
 }
